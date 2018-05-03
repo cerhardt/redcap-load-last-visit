d9d91e09285143a2713b2bbe42afa62490217299
Christian Erhardt <christian.erhardt@uni-tuebingen.de>
2018-05-03 11:09:28 +0200

    performance tweaks: load configuration only once

--- LoadLastVisitData.php
+++ LoadLastVisitData.php
@@ -4,6 +4,10 @@ namespace HIH\LoadLastVisitData;
 use ExternalModules\AbstractExternalModule;
 use ExternalModules\ExternalModules;
 
+use \REDCap as REDCap;
+use \Records as Records;
+use \DateTimeRC as DateTimeRC;
+
 class LoadLastVisitData extends AbstractExternalModule {
 
     function hook_data_entry_form_top($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance) {
@@ -12,18 +16,22 @@ class LoadLastVisitData extends AbstractExternalModule {
             
             global $Proj, $lang, $user_rights;
 
-            // only valid for longitudinal projects
-            //if (!$Proj->longitudinal) return;
-
             // load data only if user has edit permission for instrument / survey
             if ($user_rights['forms'][$instrument] != '1' && $user_rights['forms'][$instrument] != '3') {
                 return;
             }
             
-            if (in_array($instrument,$this->getProjectSetting('forms'))) {
+            // load module configuration
+            $aModConfigRaw = $this->getProjectSettings($project_id);
+            $aModConfig = array();
+            foreach($aModConfigRaw as $sKey => $aTmp) {
+                $aModConfig[$sKey] = $aTmp['value'];
+            }
+
+            if (in_array($instrument,$aModConfig['forms'])) {
     		
                 // get status array for current record
-                $grid_form_status_temp = \Records::getFormStatus($project_id,array($record));
+                $grid_form_status_temp = Records::getFormStatus($project_id,array($record));
                 $grid_form_status = $grid_form_status_temp[$record];
 
                 // $bLoadData => true if status for current form is empty
@@ -35,40 +43,42 @@ class LoadLastVisitData extends AbstractExternalModule {
                 // load data only if status is empty
                 if ($bLoadData) {
                     if ($Proj->longitudinal) {
-                        $val_type = $Proj->metadata[$this->getProjectSetting('visit_date')]['element_validation_type'];
+                        $val_type = $Proj->metadata[$aModConfig['visit_date']]['element_validation_type'];
                         if (substr($val_type, 0, 4) !== 'date') return;
                     }
                                         
                     //  all fields to load
                     $aVisitFields = array();
                     // record_id
-                    $aVisitFields[] = \REDCap::getRecordIdField();
+                    $aVisitFields[] = REDCap::getRecordIdField();
                     // date field
                     if ($Proj->longitudinal) {
-                        $aVisitFields[] = $this->getProjectSetting('visit_date');
+                        $aVisitFields[] = $aModConfig['visit_date'];
                     }
-                    foreach($Proj->metadata as $sField => $aTmp) {
-                        // load all fields of current form
-                        if ($aTmp['form_name'] == $instrument) {
+                    // load all fields of current form
+                    $aInstrumentFields = REDCap::getFieldNames($instrument);
+
+                    foreach($aInstrumentFields as $sField) {
                             // skip some fields that should be empty 
-                            if (in_array($sField,$this->getProjectSetting('excluded_fields'))) continue;
+                            if (in_array($sField,$aModConfig['excluded_fields'])) continue;
                             $aVisitFields[] = $sField;
-                        }
                     }
+
                     // get event name by event_id
-                    $sEventName = \Event::getEventNameById(intval($project_id), $event_id);
+                    $sEventName = REDCap::getEventNames(true, true, $event_id);
+
                     // array contains last filled in data
                     $aLastGood = array();
 
                     // load all events for the current record
                     if ($Proj->longitudinal) {
-                        $aDataDiag = json_decode(\REDCap::getData($project_id, 'json', $record, $aVisitFields, null, null, false, false, false, false, false, false, false, false, false, array($this->getProjectSetting('visit_date') => 'ASC')),true);
+                        $aDataDiag = json_decode(REDCap::getData($project_id, 'json', $record, $aVisitFields, null, null, false, false, false, false, false, false, false, false, false, array($aModConfig['visit_date'] => 'ASC')),true);
 
                         // assign events to visit dates
                         $aEventNameDate = array();
                         foreach($aDataDiag as $aTmp) {
-                            if (strlen($aTmp[$this->getProjectSetting('visit_date')]) > 0) {
-                                $aEventNameDate[$aTmp['redcap_event_name']] = $aTmp[$this->getProjectSetting('visit_date')];
+                            if (strlen($aTmp[$aModConfig['visit_date']]) > 0) {
+                                $aEventNameDate[$aTmp['redcap_event_name']] = $aTmp[$aModConfig['visit_date']];
                             }
                         }
                         // create new array with visits and fill in empty visit_date (necessary for forms with repeating instances because visit_date will be empty for repeating instance)
@@ -76,14 +86,14 @@ class LoadLastVisitData extends AbstractExternalModule {
                         foreach($aDataDiag as $aTmp) {
                             // skip visits with empty date
                             if (strlen($aEventNameDate[$aTmp['redcap_event_name']]) == 0) continue;
-                            if (strlen($aTmp[$this->getProjectSetting('visit_date')]) == 0) {
-                                $aTmp[$this->getProjectSetting('visit_date')] = $aEventNameDate[$aTmp['redcap_event_name']];
+                            if (strlen($aTmp[$aModConfig['visit_date']]) == 0) {
+                                $aTmp[$aModConfig['visit_date']] = $aEventNameDate[$aTmp['redcap_event_name']];
                             }
                             $aDataDiagNew[$aEventNameDate[$aTmp['redcap_event_name']]][] = $aTmp;
                         }
                         // sort by date (ascending)
                         ksort($aDataDiagNew, SORT_STRING);
-    
+
                         foreach($aDataDiagNew as $aVisit) {
                             foreach($aVisit as $aData) {
                         
@@ -93,9 +103,9 @@ class LoadLastVisitData extends AbstractExternalModule {
                                 }                
                                 
                                 // load data if status of form matches the project setting
-                                if (in_array($aData[$instrument.'_complete'],$this->getProjectSetting('load_status')) && strlen($aData[$instrument.'_complete']) > 0) {
-                                    $aLastGoodDate = \DateTimeRC::datetimeConvert($aData[$this->getProjectSetting('visit_date')], 'ymd', substr($val_type, -3));
-                                    unset($aData[$this->getProjectSetting('visit_date')]);
+                                if (in_array($aData[$instrument.'_complete'],$aModConfig['load_status']) && strlen($aData[$instrument.'_complete']) > 0) {
+                                    $aLastGoodDate = DateTimeRC::datetimeConvert($aData[$aModConfig['visit_date']], 'ymd', substr($val_type, -3));
+                                    unset($aData[$aModConfig['visit_date']]);
                                     unset($aData['redcap_event_name']);
                                     $aLastGood = $aData;
                                 }
@@ -116,10 +126,10 @@ class LoadLastVisitData extends AbstractExternalModule {
                               }                
                               
                               // load data if status of form matches the project setting
-                              if (in_array($aData[$instrument.'_complete'],$this->getProjectSetting('load_status')) && strlen($aData[$instrument.'_complete']) > 0) {
-                                  $aLastGoodDate = \DateTimeRC::datetimeConvert($aData[$this->getProjectSetting('visit_date')], 'ymd', substr($val_type, -3));
+                              if (in_array($aData[$instrument.'_complete'],$aModConfig['load_status']) && strlen($aData[$instrument.'_complete']) > 0) {
+                                  $aLastGoodDate = DateTimeRC::datetimeConvert($aData[$aModConfig['visit_date']], 'ymd', substr($val_type, -3));
                                   $aLastGoodDate .= ' ('.$lang['data_entry_278'].$aData['redcap_repeat_instance'].')';
-                                  unset($aData[$this->getProjectSetting('visit_date')]);
+                                  unset($aData[$aModConfig['visit_date']]);
                                   unset($aData['redcap_event_name']);
                                   $aLastGood = $aData;
                               }
@@ -127,7 +137,7 @@ class LoadLastVisitData extends AbstractExternalModule {
                         }
 
                     } else {
-                        $aDataDiag = json_decode(\REDCap::getData($project_id, 'json', $record, $aVisitFields),true);
+                        $aDataDiag = json_decode(REDCap::getData($project_id, 'json', $record, $aVisitFields),true);
                         $aVisit = array();
                         foreach($aDataDiag as $aTmp) {
                             if (strlen($aTmp['redcap_repeat_instance']) > 0) {
@@ -144,7 +154,7 @@ class LoadLastVisitData extends AbstractExternalModule {
                             }                
                             
                             // load data if status of form matches the project setting
-                            if (in_array($aData[$instrument.'_complete'],$this->getProjectSetting('load_status')) && strlen($aData[$instrument.'_complete']) > 0) {
+                            if (in_array($aData[$instrument.'_complete'],$aModConfig['load_status']) && strlen($aData[$instrument.'_complete']) > 0) {
                                 $aLastGoodDate = ' ('.$lang['data_entry_278'].$aData['redcap_repeat_instance'].')';
                                 $aLastGood = $aData;
                             }
@@ -155,7 +165,7 @@ class LoadLastVisitData extends AbstractExternalModule {
                     if (count($aLastGood) > 0) {
                     
                         $aData2 = $aLastGood;
-                        $aData2[\REDCap::getRecordIdField()] = $record;
+                        $aData2[REDCap::getRecordIdField()] = $record;
                         if ($Proj->longitudinal) {
                             $aData2['redcap_event_name'] = $sEventName; 
                         }
@@ -164,13 +174,13 @@ class LoadLastVisitData extends AbstractExternalModule {
                             $aData2['redcap_repeat_instance'] = $repeat_instance;
                         }
                         // set status to project setting "save_status"
-                        $aData2[$instrument.'_complete'] = $this->getProjectSetting('save_status');
+                        $aData2[$instrument.'_complete'] = $aModConfig['save_status'];
                         $data = json_encode(array($aData2));
                         // save data
-                        \REDCap::saveData($project_id, 'json', $data, 'overwrite', 'YMD', 'flat', $group_id, true);
+                        REDCap::saveData($project_id, 'json', $data, 'overwrite', 'YMD', 'flat', $group_id, true);
                     
                         // show message that data was loaded from old visit
-                        print ('<br /><div class="red">'.\REDCap::escapeHtml($this->getProjectSetting('show_message')).'<br />'.$aLastGoodDate.'</div><br />');     
+                        print ('<br /><div class="red">'.REDCap::escapeHtml($aModConfig['show_message']).'<br />'.$aLastGoodDate.'</div><br />');     
                     }        
                 }
             }
